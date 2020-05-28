e542dcb35bbad382c26a12f4f1cbcda3fd90493b
Domagoj Grgić <domagoj.grgic@outlook.com>
2020-05-28 15:51:46 +0200

    commit initial

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <?php
 // include config
 require('config.php');
+
 require('classes/bootstrap.php');
 require('classes/controller.php');
+require('classes/model.php');
+
 require('controllers/home.php');
 require('controllers/shares.php');
 require('controllers/users.php');
+
+require('models/home.php');
+require('models/share.php');
+require('models/user.php');
 
 $bootstrap=new Bootstrap($_GET);
 $controller=$bootstrap->createController();
